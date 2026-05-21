948672da6b988c427a8ad1be6d62947a3f509295
Benjamin Krause <benjamin@krause.no>
2026-05-21 15:16:27 +0200

    Improve config flow so the user has to select the correct address from a list

--- custom_components/min_renovasjon/config_flow.py
+++ custom_components/min_renovasjon/config_flow.py
@@ -14,25 +14,23 @@ class GarbageCalendarConfigFlow(config_entries.ConfigFlow, domain=const.DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
+    def __init__(self):
+        self._candidate_addresses = []
+
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         errors = {}
         address = None
 
         if user_input is not None:
             try:
                 address = user_input["address"]
-                error, address_info = await self._get_address_info(address)
+                error, addresses = await self._address_lookup(address)
 
                 if error is not None:
                     errors["base"] = error
                 else:
-                    # Test if the address is supported by making a garbage collection data request
-                    error, is_supported = await self._test_address_support(address_info)
-                    if not is_supported:
-                        errors["base"] = error
-                    else:
-                        return self.async_create_entry(title="Min Renovasjon", data=address_info)
+                    self._candidate_addresses = addresses
+                    return await self.async_step_select_address()
 
             except Exception:
                 _LOGGER.exception("Unexpected exception")
@@ -46,33 +44,63 @@ class GarbageCalendarConfigFlow(config_entries.ConfigFlow, domain=const.DOMAIN):
             errors=errors
         )
 
-    async def _get_address_info(self, address_search_string):
-        """Get address information based on user input."""
-        error, address_info = await self._address_lookup(address_search_string)
+    async def async_step_select_address(self, user_input=None):
+        errors = {}
 
-        if error is not None:
-            return error, None
+        if user_input is not None:
+            try:
+                new_search = (user_input.get("new_search") or "").strip()
+                if new_search:
+                    error, addresses = await self._address_lookup(new_search)
+                    if error is not None:
+                        errors["base"] = error
+                    else:
+                        self._candidate_addresses = addresses
+                        return await self.async_step_select_address()
+                elif user_input.get("address") is not None:
+                    idx = int(user_input["address"])
+                    address_info = self._build_address_info(self._candidate_addresses[idx])
+                    error, is_supported = await self._test_address_support(address_info)
+                    if not is_supported:
+                        errors["base"] = error
+                    else:
+                        return self.async_create_entry(title="Min Renovasjon", data=address_info)
+                else:
+                    errors["base"] = "select_or_search"
+            except Exception:
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
 
-        if address_info is not None:
-            return None, {
-                const.CONF_STREET_NAME: address_info[0],
-                const.CONF_STREET_CODE: str(address_info[1]),
-                const.CONF_HOUSE_NUMBER: str(address_info[2]),
-                const.CONF_MUNICIPALITY_NUMBER: str(address_info[4]),
-                const.CONF_APP_KEY: const.DEFAULT_APP_KEY
-            }
+        options = {
+            str(i): f"{a['adressenavn']} {a['nummer']}, {a['postnummer']} {a['poststed']}"
+            for i, a in enumerate(self._candidate_addresses)
+        }
+        return self.async_show_form(
+            step_id="select_address",
+            data_schema=vol.Schema({
+                vol.Optional("address"): vol.In(options),
+                vol.Optional("new_search"): str,
+            }),
+            errors=errors,
+        )
 
-        return "no_address_found", None
+    def _build_address_info(self, addr):
+        return {
+            const.CONF_STREET_NAME: addr["adressenavn"],
+            const.CONF_STREET_CODE: str(addr["adressekode"]),
+            const.CONF_HOUSE_NUMBER: str(addr["nummer"]),
+            const.CONF_MUNICIPALITY_NUMBER: str(addr["kommunenummer"]),
+            const.CONF_APP_KEY: const.DEFAULT_APP_KEY,
+        }
 
     async def _address_lookup(self, search_string: str):
-        """Make an API call to get address details."""
         regex = r"(.*ve)(i|g)(.*)"
         subst = "\\1*\\3"
         search_string = re.sub(regex, subst, search_string, 0, re.MULTILINE)
 
         params = {
             "sok": search_string,
-            # Specify fields to return, modify as necessary
+            "treffPerSide": 10,
             "filtrer": "adresser.kommunenummer,"
                        "adresser.adressenavn,"
                        "adresser.adressekode,"
@@ -87,64 +115,48 @@ class GarbageCalendarConfigFlow(config_entries.ConfigFlow, domain=const.DOMAIN):
                 response = await resp.read()
                 if resp.ok:
                     data = json.loads(response.decode("UTF-8"))
+                    addresses = data["adresser"]
 
-                    if not data["adresser"]:
+                    if not addresses:
                         return "no_address_found", None
 
-                    if len(data["adresser"]) > 1:
-                        return "multiple_addresses_found", None
+                    if data.get("metadata", {}).get("totaltAntallTreff", len(addresses)) > 10:
+                        return "too_many_addresses", None
 
-                    # Extract address details from the API response
-                    return None, (
-                        data["adresser"][0]["adressenavn"],  # Street name
-                        data["adresser"][0]["adressekode"],   # Street code
-                        data["adresser"][0]["nummer"],        # House number
-                        data["adresser"][0]["kommunenavn"],   # Municipality name
-                        data["adresser"][0]["kommunenummer"],  # Municipality number
-                        data["adresser"][0]["postnummer"],     # Postal code
-                        data["adresser"][0]["poststed"],       # Postal locality
-                    )
+                    return None, addresses
 
         return "municipality_not_customer", None
 
     async def _test_address_support(self, address_info):
-        """Make a test request to check if the address is supported."""
-        kommunenr = address_info[const.CONF_MUNICIPALITY_NUMBER]
-        gatenavn = address_info[const.CONF_STREET_NAME]
-        gatekode = address_info[const.CONF_STREET_CODE]
-        husnr = address_info[const.CONF_HOUSE_NUMBER]
+        municipality_number = address_info[const.CONF_MUNICIPALITY_NUMBER]
+        street_name = address_info[const.CONF_STREET_NAME]
+        street_code = address_info[const.CONF_STREET_CODE]
+        house_number = address_info[const.CONF_HOUSE_NUMBER]
         app_key = const.DEFAULT_APP_KEY
 
-        # Construct the target URL for the garbage collection API
         target_url = (
             f"https://komteksky.norkart.no/MinRenovasjon.Api/api/tommekalender/"
-            f"?kommunenr={kommunenr}&gatenavn={gatenavn}&gatekode={gatekode}&husnr={husnr}"
+            f"?kommunenr={municipality_number}&gatenavn={street_name}&gatekode={street_code}&husnr={house_number}"
         )
-
-        # Construct the URL for the proxy server
         url = f"{const.PROXY_SERVER_URL}{target_url}"
-
         headers = {
             "RenovasjonAppKey": app_key,
-            "Kommunenr": kommunenr
+            "Kommunenr": municipality_number
         }
 
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, headers=headers) as response:
-                    # Get the response content as text
-                    response_content = await response.text()  
-                    
+                    response_content = await response.text()
+
                     if response.status != 200:
                         return "municipality_not_customer", False
 
-                    # Try to decode as JSON regardless of content type
                     try:
                         calendar_data = await response.json()
                     except aiohttp.ContentTypeError:
                         calendar_data = json.loads(response_content)
 
-                    # If the result is an empty list
                     if not calendar_data:
                         return "municipality_not_customer", False
 
